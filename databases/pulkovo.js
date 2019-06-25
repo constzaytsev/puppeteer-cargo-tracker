e9f650c4b780378cd09594acd34f7753253eff03
Zaitsumi <zaitsumi@gmail.com>
2019-06-25 11:36:23 +0300

    feat: logs added

--- databases/pulkovo.js
+++ databases/pulkovo.js
@@ -1,27 +1,27 @@
 import sanitizeHtml from 'sanitize-html';
 import minify from 'html-minifier';
 
-export default async (page, cargoPrefix, cargoNumber) => {
+export default (page, cargoPrefix, cargoNumber) => new Promise(async (resolve, reject) => {
   await page.goto('https://pulkovo-cargo.ru/clients/dispatch', { waitUntil: 'domcontentloaded' });
-  await page.evaluate((cargoPrefix, cargoNumber) => {
+  console.log('https://pulkovo-cargo.ru/clients/dispatch opened');
+  await page.evaluate(() => {
     document.querySelector('#blankPrefix').value = cargoPrefix;
     document.querySelector('#blankNumber').value = cargoNumber;
   }, cargoPrefix, cargoNumber);
   const inputElement = await page.$('button#get-tracking');
+  console.log(`submit button ${inputElement}`);
   await Promise.all([
     page.waitForResponse('https://pulkovo-cargo.ru/site/tracking'),
     inputElement.click(),
   ]);
-
+  console.log('get response from https://pulkovo-cargo.ru/site/tracking');
   if (await page.$('.tracking-table') !== null) {
     const html = await page.evaluate(() => document.querySelector('#response').innerHTML);
-    return {
+    resolve({
       success: minify(sanitizeHtml(html, { allowedAttributes: { '*': ['colspan'] } }), {
         collapseWhitespace: true,
       }),
-    };
+    });
   }
-  return {
-    error: 'По данному номеру ничего не найдено',
-  };
-};
+  reject(new Error('По данному номеру ничего не найдено'));
+});
--- index.js
+++ index.js
@@ -6,6 +6,8 @@ import * as Sentry from '@sentry/node';
 import Aeroflot from './databases/aeroflot';
 import Pulkovo from './databases/pulkovo';
 
+require('dotenv').config();
+
 Sentry.init({ dsn: 'https://4ea34bda20f4461386d47ea142c87a36@sentry.io/1371072' });
 
 const app = express();
@@ -13,20 +15,29 @@ app.use(Sentry.Handlers.requestHandler());
 app.use(cors());
 
 app.get('/track', async (req, res) => {
+  console.log('/track handeled');
+
   const cargoPrefix = req.query.prefix;
   const cargoNumber = req.query.number;
 
   if (!cargoPrefix) return res.json({ result: { error: 'Не указан префикс груза' } });
   if (!cargoNumber) return res.json({ result: { error: 'Не указан номер груза' } });
 
+  console.log(`connecting to ${process.env.PUPPETEER_HOST}`);
+
   const browser = await puppeteer.connect({
     browserWSEndpoint: process.env.PUPPETEER_HOST,
   }).catch((err) => { throw err; });
 
+  console.log('browser created');
+
   const page = await browser.newPage();
+
+  console.log('new page opened');
+
   const result = cargoPrefix == 555
     ? await Aeroflot(page, cargoPrefix, cargoNumber)
-    : await Pulkovo(page, cargoPrefix, cargoNumber);
+    : await Pulkovo(page, cargoPrefix, cargoNumber).catch((error) => { console.log(error); });
   res.json({
     result,
   });
