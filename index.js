4c12cda7bb4a8b5928988c0008967f2aa19a2c61
Konstantin Zaytsev <zaytsev.const@gmail.com>
2021-04-23 10:59:38 +0300

    chore: heroku test

--- index.js
+++ index.js
@@ -1,27 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import { Promise } from 'bluebird';
-import * as Sentry from '@sentry/node';
-import cache from './plugins/cache';
+// import * as Sentry from '@sentry/node';
+// import cache from './plugins/cache';
 
-import Sheremetevo from './databases/sheremetevo';
+// import Sheremetevo from './databases/sheremetevo';
 import Pulkovo from './databases/pulkovo';
-import Domodedovo from './databases/domodedovo';
-import Moscow from './databases/moscow';
+// import Domodedovo from './databases/domodedovo';
+// import Moscow from './databases/moscow';
 
 const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-Sentry.init({
-  dsn: 'https://4ea34bda20f4461386d47ea142c87a36@o183199.ingest.sentry.io/1371072',
-  environment: 'dev',
-});
+// Sentry.init({
+//   dsn: 'https://4ea34bda20f4461386d47ea142c87a36@o183199.ingest.sentry.io/1371072',
+//   environment: 'dev',
+// });
 
-app.use(Sentry.Handlers.requestHandler());
+// app.use(Sentry.Handlers.requestHandler());
 app.use(cors());
 
-app.get('/track', cache(600), async (req, res) => {
+app.get('/track', async (req, res) => {
   const cargoPrefix = req.query.prefix;
   const cargoNumber = req.query.number;
 
@@ -31,9 +31,9 @@ app.get('/track', cache(600), async (req, res) => {
   try {
     const result = await Promise.any([
       Pulkovo(cargoPrefix, cargoNumber),
-      Sheremetevo(cargoPrefix, cargoNumber),
-      Domodedovo(cargoPrefix, cargoNumber),
-      Moscow(cargoPrefix, cargoNumber),
+      // Sheremetevo(cargoPrefix, cargoNumber),
+      // Domodedovo(cargoPrefix, cargoNumber),
+      // Moscow(cargoPrefix, cargoNumber),
     ]);
     return res.json({
       result: {
@@ -41,9 +41,9 @@ app.get('/track', cache(600), async (req, res) => {
       },
     });
   } catch (e) {
-    e.forEach((error) => {
-      if (error.name !== 'NoResults') Sentry.captureException(error);
-    });
+    // e.forEach((error) => {
+    //   if (error.name !== 'NoResults') Sentry.captureException(error);
+    // });
 
     return res.json({
       result: {
